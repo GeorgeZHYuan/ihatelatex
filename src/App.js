ceae1e5a4f83701e79bd8f0333d54c86e3b7adcd
George Zhihua Yuan <yuangeorge9981@gmail.com>
2021-11-24 23:50:24 -0500

    Resume Base Setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
+import React from 'react';
+import styled from 'styled-components'
+
+const S = {}
+
+S.Resume = styled.div`
+  border-style: solid;
+
+  > div {
+    -webkit-print-color-adjust:exact;
+  }
+`
+S.Title = styled.div`
+`
+S.Section = styled.div`
+`
+
+const Title = () => {
+  
+}
+
 const App = () => {
   return (
-    <div>
-      Test
-    </div>
+    <S.Resume>
+      <S.Title>
+        George Yuan
+      </S.Title>
+      <S.Section>
+        Work Experience
+      </S.Section>
+      <S.Section>
+        Noteable Projects
+      </S.Section>
+      <S.Section>
+        Skills
+      </S.Section>
+    </S.Resume>
   );
 }
 
---
+++ src/README.md
@@ -0,0 +1,3 @@
+# How to Generate PDF
+
+- run the code and print the page as pdf
