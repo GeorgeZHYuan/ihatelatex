05f9e155e1666cddaf72f38018f5ac81984e7938
George Zhihua Yuan <yuangeorge9981@gmail.com>
2022-01-27 17:34:55 -0500

    typo fix

--- src/data.js
+++ src/data.js
@@ -6,10 +6,10 @@ export const workExperience = [
     date: `Sept – Dec 2021`,
     address: `Toronto, ON`,
     details: [
-      `Built and maintained data analytics tools (React, Redux), microservices (SpringBoot, Flask) and CI/CD pipelines, satisfying user requests and improving system performance`,
-      `Redesigned search suggestion system, updating error handling, database queires, and business logic, resulting in 75% more revelancy in results and resolving all crashes from the search dropdown API`,
-      `Dockerized and deployed Selenium test suite to EKS, automated crash recovery with helm liveness probes, and created a parameterized Jenkins pipeline, setting up the infrastructure for reccurent test runs`,
-      `Investigated incorrect primary/foreign key mappings, developed cleanup and functional testing procedures, and rolled out changes to development and production enviroments`,
+      `Built web features and CI/CD pipelines as full stack developer to maintain and improve data analytics tools (React, Redux), microservices (SpringBoot, Flask) and development workflows`,
+      `Redesigned backend infrastructure for error handling, database queries, and search suggestion business logic, resolving all search related crashes while increasing search relevancy by over 80%`,
+      `Dockerized and deployed Selenium test suite to EKS, automated crash recovery with helm liveness probes, and created a parameterized Jenkins pipeline, enabling automated recurrent test runs`,
+      `Investigated incorrect primary/foreign key mappings, developed cleanup and functional testing procedures, and rolled out changes to development and production environment reducing 100% of mapping errors`,
     ]
   },
   {
@@ -18,9 +18,9 @@ export const workExperience = [
     date: `Jan – April 2021`,
     address: `Toronto, ON`,
     details: [
-      `Led the end-to-end development of a code-viewer feature, organized meetings to discuss/review trade-offs for several product designs and backend infrastructures, and developed core functionality based off design documents`,
+      `Led the end-to-end development of a code-viewer feature, organized meetings to discuss/review trade-offs for several product and architectural designs, and implemented full functionality from created design documents`,
       `Developed server status tracking tool, used recursive algorithms and React libraries to generate accordion layout from JSON metadata, and provided live updates through mapped Redux states`,
-      `Designed and built asynchronous REST API endpoints, managing multiple redirections over microservice architecture, and authenticated by central LDAP network`
+      `Designed and built asynchronous REST API endpoints, managing multiple redirections over microservice architecture`
     ]
   },
   {
@@ -29,9 +29,9 @@ export const workExperience = [
     date: `Sept 2019 – Dec 2019`,
     address: `Waterloo, ON`,
     details: [
-      `Implemented user interfaces and warning/detection systems against slipping, component detachment and crashing for autonomous robot using C++ (OOP), Python and JavaScript`,
-      `Developed robot human communication systems, collaborated with product team to determine audio behavior, and a built state machine for multi-threaded management of robot states and audio resources`,
-      `Performed data analysis from field test data and live feeds via sockets from ROS publisher/subscriber framework`,
+      `Implemented user interfaces and warning/detection systems against slipping, component detachment and crashing for autonomous robot using C++, Python and JavaScript`,
+      `Developed robot human communication systems, collaborated with product team to determine audio behavior, and built a state machine for multithreaded management of robot states and audio resources`,
+      `Analyzed data collected through field tests and live feeds using TCP sockets from ROS publisher/subscriber framework`,
     ]
   },
   {
@@ -40,7 +40,7 @@ export const workExperience = [
     date: `May 2018 – Aug 2018`,
     address: `Calgary, AB`,
     details: [
-      `Coded a SAML based single sign-on middleware using PassportJs with cached JWT Tokens for statelessness and scalability, allowing client identity providers to authenticate with an internal IBM service provider`,
+      `Built a SAML based single sign-on middleware using PassportJs with cached JWT Tokens for statelessness and scalability, allowing client identity providers to authenticate with an internal IBM service provider`,
       `Digitized an oil task authorization system using React forms, NodeJs backend verification, and a NoSQL db on IBM cloud, reducing client document approval times by several weeks`
     ]
   }
